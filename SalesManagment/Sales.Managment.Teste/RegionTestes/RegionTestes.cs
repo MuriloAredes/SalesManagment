aa7142bc26f0e7c64b6b6b980170287aaa721c12
Murilo Aredes <murilo.aredes.prestserv@petrobras.com.br>
2023-03-01 15:05:53 -0300

    adicionando  classe de teste

---
+++ SalesManagment/Sales.Managment.Teste/RegionTestes/RegionTestes.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using SalesManagment.Contract.RegionContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Managment.Teste.RegionTestes
+{
+    [TestClass]
+    public class RegionTestes
+    {
+        private readonly IMediator _mediator;
+
+        public RegionTestes(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [TestMethod]
+        public  void UpdateRegion() 
+        {
+            var request = new RegionRequest(true);
+
+            var result =  _mediator.Send(request);
+
+            Assert.AreEqual("Base de dados atualizada com sucesso", result);
+        }
+    }
+}
